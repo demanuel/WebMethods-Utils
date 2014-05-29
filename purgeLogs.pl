68f6bd2eabd402165e6208cac56a09e799d402fc
David Santiago <demanuel@ymail.com>
2014-05-29 07:56:04 +0200

    replaced qx for backticks

--- purgeLogs.pl
+++ purgeLogs.pl
@@ -3,7 +3,6 @@
 # Author: David Santiago
 # This program is licensed under GPLv3 license
 #
-# purge Logs  Copyright (C) 2014  David Santiago
 
 use v5.8.8;
 use Getopt::Long;
@@ -12,10 +11,10 @@ my $logs_dir = '.';
 
 my $lsof = '/usr/sbin/lsof';
 
-GetOptions("lsof=s"=>\$lsof,"dir=s"=>\$logs_dir);
+GetOptions("lsof=s"=>\$lsof,"dir=s"=>\$logs_dir);  
 
+@output = `$lsof *`;
 
-@output = qx '$lsof *';
 shift @output; #remove the headers of the lsof output
 foreach(@output){
         chomp($line = $_);
@@ -35,6 +34,5 @@ closedir DIR;
 
 
 $command = 'nohup bzip2 '.join(' ', @logs).' &';
-
 
 system($command);
